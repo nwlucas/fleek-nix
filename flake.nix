05cc55be74db8003738674447a3e19fb8b323bd2
Nigel Williams-Lucas <yesniah@gmail.com>
2024-03-30 23:17:09 -0400

    join new system

--- flake.nix
+++ flake.nix
@@ -52,6 +52,32 @@
         ];
       };
       
+      "nwilliams-lucas@NWL-MBM2" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./NWL-MBM2/nwilliams-lucas.nix
+          ./NWL-MBM2/custom.nix
+          # self-manage fleek
+          {
+            home.packages = [
+              fleek.packages.aarch64-darwin.default
+            ];
+          }
+          ({
+           nixpkgs.overlays = [];
+          })
+
+        ];
+      };
+      
     };
   };
 }
