b4a584e5002b733897bcd185b8ef55911ce904e5
Nigel Williams-Lucas <yesniah@gmail.com>
2024-03-31 00:48:38 -0400

    add packages: jq

--- NWL-MBM2/custom.nix
+++ NWL-MBM2/custom.nix
@@ -1,5 +1,20 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
+  system = {
+    defaults = {
+      dock = {
+        autohide = true;
+        mru-spaces = false;
+        orientation = "bottom";
+      };
+
+      finder = {
+        AppleShowAllExtensions = true;
+        QuitMenuItem = true;
+        FXEnableExtensionChangeWarning = false;
+      };
+    };
+  };
  
 }
